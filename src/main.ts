82e26808893726b77f202de8aaa954cb127b586b
Santiago Alejandro Gaviria Murcia <69436478+santialejo97@users.noreply.github.com>
2022-09-27 11:11:38 -0500

    Front de socket Finalizado

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,27 @@ import { wsClient } from './ws-clients';
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
-    <h1>Seccion de socket</h1>
+    <h2>Seccion de socket</h2>
+    <input id="jwToken" placeholder="Json Web Token" />
+    <button id="btn-connect">Connect</button>
+    <br>
+   <span id="connectWs">offline</span>
+   <ul id="listClient"></ul>
+   <form id="formulario">
+   <input id="inputMessage" placeholder="send a message"/>
+   </form>
+   <h3>Messages</h3>
+   <ul id="listMessages">
+   </ul>
   </div>
 `
-wsClient()
+// wsClient()
+const inputToken = document.querySelector<HTMLInputElement>('#jwToken')!
+const btnConnect = document.querySelector<HTMLButtonElement>('#btn-connect')!
+
+btnConnect.addEventListener('click', () => {
+  if(inputToken.value.trim().length <= 0) return alert('Enter at valid JWT')
+  wsClient(inputToken.value.trim())
+})
+
 
--- src/ws-clients.ts
+++ src/ws-clients.ts
@@ -1,3 +1,59 @@
-export const wsClient = ()=>{
-    console.log('hola buenas tardes')
+import { Manager, Socket } from "socket.io-client"
+let socket : Socket
+export const wsClient = (jwToken: string)=>{
+    const manager = new Manager('http://localhost:3000/socket.io/socket.io.js', {
+        extraHeaders: {
+            authentication: jwToken
+        }
+    })
+    socket?.removeAllListeners()
+    socket =  manager.socket('/')
+    addListener()
+}
+
+
+const addListener = () => {
+
+
+    const span = document.querySelector('#connectWs')!;
+    const listClient = document.querySelector('#listClient')!;
+    const form = document.querySelector<HTMLFormElement>('#formulario')!
+    const message = document.querySelector<HTMLInputElement>('#inputMessage')!
+    const listMessages = document.querySelector('#listMessages')!
+
+    socket.on('connect', () =>{
+        span.textContent = 'Online'; 
+    })
+
+    socket.on('disconnect', () =>{
+        span.textContent = 'Offline';
+    })
+
+    socket.on('clients-updated', (listClients: string[]) =>{
+         listClients.forEach(client => {
+             const li = document.createElement('LI');
+             li.innerHTML = client
+            listClient.appendChild(li)
+        })
+     })
+    
+    form.addEventListener('submit', (event) => {
+        event.preventDefault()
+        if ( message.value.trim().length <= 0 ) return
+        console.log(message.value)
+        socket.emit('message', { id: 'yo', message: message.value.trim() })
+        message.value = ''
+    })
+
+
+    socket.on('message-serve', (payload: { fullName: string, message: string }) => {
+        const messageLi = document.createElement('LI');
+        messageLi.innerHTML = `${payload.fullName} - ${payload.message}`
+        listMessages.appendChild(messageLi)
+    })
+
+
+    
+    
+
 }
